9fead179fa8d950f6022678bc3eb522c35048c51
linye1028 <linye1028@qq.com>
2019-09-27 12:00:30 +0800

    second

---
+++ 3music/32happyBirthdaySongWithoutDelay/32happyBirthdaySongWithoutDelay.ino
@@ -0,0 +1,54 @@
+#define L1 262
+#define L2 294
+#define L3 330
+#define L4 349
+#define L5 392
+#define L6 440
+#define L7 494
+#define M1 523
+#define M2 587
+#define M3 659
+#define M4 698
+#define M5 784
+#define M6 880
+#define M7 968
+#define H1 1046
+#define H2 1175
+#define H3 1318
+#define H4 1397
+#define H5 1568
+#define H6 1760
+#define H7 1967
+#define M0 0
+unsigned int song[28]={M5,M5,M6,M5,H1,M7,
+                       M5,M5,M6,M5,H2,
+                       L1,M5,M5,H5,H3,H1,M7,M6,M0,M0,H4,H4,
+                       H3,H1,H2,H1,H1
+                      };
+unsigned char time1[28]={1,1,2,2,2,4,
+					   1,1,2,2,2,
+					   2,1,1,2,2,2,2,4,2,2,1,1,
+					   1,1,1,6,6,
+					  };
+void setup() {
+	// put your setup code here, to run once:
+	pinMode(8,OUTPUT);
+}
+unsigned song_i=0;
+unsigned long preMs=0;
+void loop() {
+	// put your main code here, to run repeatedly:
+	if(millis()-preMs>=200*time1[song_i])
+	{
+		noTone(8);
+		preMs=millis();
+		if(song[song_i]!=M0)  
+			tone(8,song[song_i]);
+		song_i++;
+		if(song_i>=28)
+		{
+			song_i=0; 
+			noTone(8);
+		}
+	}
+}
